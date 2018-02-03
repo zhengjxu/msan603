5072195fe540155d04543a0e166eeec2626c956c
zhengjxu <zxu38@dons.usfca.edu>
2018-02-02 22:24:33 -0800

    add file

---
+++ process.py
@@ -0,0 +1,44 @@
+import os
+import json
+    
+def extract_json(json_line):
+    flag = 0
+    name, age = '', ''
+    try:
+        a_dict = json.loads(json_line)
+        # check present
+        name, age = a_dict['name'], a_dict['prop']['age']
+    except Exception as e:
+        print('having error load json line: %s' % e)
+        flag = 1
+    
+    # check non-empty
+    if name=='' or age=='': flag=1
+    
+    return '%s\t%s' % (name, age), flag
+
+def process_file(prefix, input_dir='/srv/runme', output_dir = '/srv/runme'):
+    output_file = '%s/%s.txt' % (output_dir, prefix)
+    if os.path.exists(output_file): os.remove(output_file)
+    
+    result = []
+    
+    files = os.listdir(input_dir)
+    files = [f for f in files if f.startswith(prefix)]
+    for file in files:
+        file_path = input_dir+'/'+file
+        print('process %s' % file_path) 
+        f = open(file_path)
+        for json_line in f:
+            json_line = json_line[:-1]
+            
+            name_age, flag = extract_json(json_line)
+            if flag==1: continue
+            
+            result.append(name_age)
+        f.close()
+    
+    f = open(output_file, 'w')
+    f.write('\n'.join(result))
+    f.close()
+    print('Done!')  
